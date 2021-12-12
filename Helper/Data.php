7f4afb4a94e2e7af107782994204633b36ed180c
joseph.metcalfe <joseph.metcalfe@stockinthechannel.com>
2021-12-12 18:53:00 +0000

    Initial commit for module

---
+++ Helper/Data.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Selectj\SpeedSuite\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Helper\Context;
+use Magento\Store\Model\ScopeInterface;
+
+class Data extends AbstractHelper
+{
+
+    const DEFER_JS_ENABLE = 'speedsuite/defer_js_load/general/enabled';
+    /**
+     * @param Context $context
+     */
+    public function __construct(Context $context
+    ){
+        parent::__construct($context);
+    }
+
+    public function getStoreConfig($configPath): bool
+    {
+        return $this->scopeConfig->getValue(
+            $configPath,
+            ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    public function isDeferJsEnabled() : bool
+    {
+        return $this->getStoreConfig(self::DEFER_JS_ENABLE);
+    }
+}
